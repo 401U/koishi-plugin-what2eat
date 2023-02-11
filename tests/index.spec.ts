b75aa93ead44e3f037f4c054ad0ff0e2260180ad
401Unauthorized <redme@live.cn>
2023-02-12 00:07:04 +0800

    chore: update

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,20 +1,24 @@
 import { App } from 'koishi'
 import mock from '@koishijs/plugin-mock'
+import memory from '@koishijs/plugin-database-memory'
 import * as what2eat from '../src'
 
 const app = new App()
 app.plugin(mock)
-app.plugin(what2eat)
+app.plugin(memory)
 
 const clientA = app.mock.client('123')
 
-before(() => app.start())
+before(() => {
+  app.start()
+  app.plugin(what2eat)
+})
 after(() => app.stop())
 
 it('should recommand food to me', async () => {
-  await clientA.shouldReply('今天吃什么', '不知道吃啥嘞')
+  await clientA.shouldReply('今天吃什么', 'apple')
 })
 
 it('should recommand drink to me', async () => {
-  await clientA.shouldReply('今天喝什么', '不知道喝啥嘞')
+  await clientA.shouldReply('今天喝什么', 'orange')
 })
