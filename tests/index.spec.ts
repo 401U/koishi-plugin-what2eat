022ecac85992343c575810a21eaaf87f309c4be0
401Unauthorized <redme@live.cn>
2023-02-11 16:46:10 +0800

    chore: code lint

--- tests/index.spec.ts
+++ tests/index.spec.ts
@@ -1,4 +1,4 @@
-import { App } from "koishi";
+import { App } from 'koishi'
 import mock from '@koishijs/plugin-mock'
 import * as what2eat from '../src'
 
@@ -8,9 +8,13 @@ app.plugin(what2eat)
 
 const clientA = app.mock.client('123')
 
-before(() => { app.start() })
-after(() => { app.stop() })
+before(() => app.start())
+after(() => app.stop())
 
-it('should work',async () => {
-    await clientA.shouldReply("今天吃什么", "不知道吃啥嘞")
+it('should recommand food to me', async () => {
+  await clientA.shouldReply('今天吃什么', '不知道吃啥嘞')
+})
+
+it('should recommand drink to me', async () => {
+  await clientA.shouldReply('今天喝什么', '不知道喝啥嘞')
 })
